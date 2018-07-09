0caf90b4c76286a00705e8116c7188607c9923ce
KanchanChauhan <angel143.dr@gmail.com>
2018-07-09 16:18:33 +0530

    Update work_order.js

--- erpnext/manufacturing/doctype/work_order/work_order.js
+++ erpnext/manufacturing/doctype/work_order/work_order.js
@@ -290,8 +290,8 @@ frappe.ui.form.on("Work Order", {
 				freeze: true,
 				callback: function(r) {
 					if(r.message) {
-						frm.set_value('sales_order', "");
-						frm.trigger('set_sales_order');
+// 						frm.set_value('sales_order', "");
+// 						frm.trigger('set_sales_order');
 						erpnext.in_production_item_onchange = true;
 						$.each(["description", "stock_uom", "project", "bom_no",
 							"allow_alternative_item", "transfer_material_against_job_card"], function(i, field) {
