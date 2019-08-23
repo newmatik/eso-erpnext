0d46c38dea9c3e2c521b639e77c99b0d232ed9a1
Kanchan Chauhan <angel143.dr@gmail.com>
2019-08-23 16:17:15 +0530

    fix(blanket-order): Terms and Conditions section

--- erpnext/manufacturing/doctype/blanket_order/blanket_order.js
+++ erpnext/manufacturing/doctype/blanket_order/blanket_order.js
@@ -6,8 +6,26 @@ frappe.ui.form.on('Blanket Order', {
 		frm.add_fetch("customer", "customer_name", "customer_name");
 		frm.add_fetch("supplier", "supplier_name", "supplier_name");
 	},
-
+	customer: function(frm) {
+		erpnext.utils.get_party_details(frm)
+	},
+	supplier: function(frm) {
+		erpnext.utils.get_party_details(frm)
+	},
+	customer_address: function(frm) {
+		erpnext.utils.get_address_display(frm, 'customer_address', 'address_display');
+	},
+	supplier_address: function(frm) {
+		erpnext.utils.get_address_display(frm, 'supplier_address', 'address_display');
+	},
+	contact_person: function(frm) {
+		erpnext.utils.get_contact_details(frm);
+	},
+	company: function(frm) {
+		frm.set_value("currency", erpnext.get_currency(frm.doc.company));
+	},
 	refresh: function(frm) {
+		frm.events.set_dynamic_labels(frm);
 		if (frm.doc.customer && frm.doc.docstatus === 1) {
 			frm.add_custom_button(__('View Orders'), function() {
 				frappe.set_route('List', 'Sales Order', {blanket_order: frm.doc.name});
@@ -31,5 +49,48 @@ frappe.ui.form.on('Blanket Order', {
 				});
 			}).addClass("btn-primary");
 		}
+	},
+	tc_name: function (frm) {
+		erpnext.utils.get_terms(frm.doc.tc_name, frm.doc, function (r) {
+			if (!r.exc) {
+				frm.set_value("terms", r.message);
+			}
+		});
+	},
+	set_dynamic_labels: function(frm) {
+		var company_currency = erpnext.get_currency(frm.doc.company);
+		cur_frm.set_df_property("conversion_rate", "description", "1 " + frm.doc.currency
+		+ " = [?] " + company_currency);
+		frm.set_currency_labels(["rate"], frm.doc.currency, "items");
+	},
+	currency: function(frm) {
+		var company_currency = erpnext.get_currency(frm.doc.company);
+		var args;
+		if (frm.doc.blanket_order_type == "Selling") {
+			args = "for_selling";
+		}
+		else {
+			args = "for_buying";
+		}
+		return frappe.call({
+			method: "erpnext.setup.utils.get_exchange_rate",
+			args: {
+				transaction_date: frm.doc.posting_date,
+				from_currency: frm.doc.currency,
+				to_currency: company_currency,
+				args: args
+			},
+			callback: function(r) {
+				frm.set_value("conversion_rate",flt(r.message));
+				frm.events.set_dynamic_labels(frm);
+			}
+		});
 	}
 });
+
+frappe.ui.form.on('Blanket Order Item', {
+	rate: function(frm, cdt, cdn){
+		var d = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "base_rate", d.rate*frm.doc.conversion_rate);
+	},
+})
