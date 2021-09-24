5ec9f45f6747b33120dd8070fe61b00c85b7f1aa
icecliff <isaiah@elexess.com>
2021-09-24 01:58:23 +0000

    feat(Hazardous Material): Hazardous Material Features

--- erpnext/stock/doctype/ghs_hazard_statement/ghs_hazard_statement.py
+++ erpnext/stock/doctype/ghs_hazard_statement/ghs_hazard_statement.py
@@ -3,8 +3,33 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+import collections
+
+from newmatik.custom_hooks.item import on_update
 
 class GHSHazardStatement(Document):
-	pass
+
+	def before_save(self):
+		newlist = frappe.db.get_list("Hazardous Material GHS Precautionary Statements", filters={"parent": self.name})
+		newStr = []	
+		oldStr = []	
+		for x in range(len(newlist)):
+			newS = str(newlist[x]).replace("{","").replace("'","").replace("name: ","").replace("}","")
+			newStr.append(newS)
+		print(newStr)	
+		for i in self.hazardous_material_ghs_precautionary_statements:
+			 oldStr.append(str(i.name))
+		print(oldStr)
+		new = set(newStr)
+		old = set(oldStr)
+		if new != old:
+			doc = frappe.db.get_list("Hazardous Material GHS Hazard Statements", {"ghs_hazard_statement": self.name },"parent")
+			for x in range(len(doc)):
+				s = str(doc[x]).replace("{","").replace("'","").replace("parent: ","").replace("}","")
+				print(s)
+				frappe.db.set_value("Hazardous Material", s, {"status": "Review"})
+		else:
+			pass
+			
