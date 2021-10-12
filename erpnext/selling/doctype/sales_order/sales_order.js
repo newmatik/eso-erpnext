baa974ce2abb923ad2a899f53fb3b51e94be9cc5
vanessa bualat <vanessa.bualat01@gmail.com>
2021-10-12 15:19:45 +0800

    refactor: added an OR condition for NCR type SO to show button

--- erpnext/selling/doctype/sales_order/sales_order.js
+++ erpnext/selling/doctype/sales_order/sales_order.js
@@ -38,161 +38,42 @@ frappe.ui.form.on("Sales Order", {
 		frm.set_df_property("packed_items", "cannot_add_rows", true);
 		frm.set_df_property("packed_items", "cannot_delete_rows", true);
 	},
-
-	refresh: function (frm) {
-		if (frm.doc.docstatus === 1) {
-			if (
-				frm.doc.status !== "Closed" &&
-				flt(frm.doc.per_delivered) < 100 &&
-				flt(frm.doc.per_billed) < 100 &&
-				frm.has_perm("write")
-			) {
-				frm.add_custom_button(__("Update Items"), () => {
-					erpnext.utils.update_child_items({
-						frm: frm,
-						child_docname: "items",
-						child_doctype: "Sales Order Detail",
-						cannot_add_row: false,
-						has_reserved_stock: frm.doc.__onload && frm.doc.__onload.has_reserved_stock,
-					});
-				});
-
-				// Stock Reservation > Reserve button should only be visible if the SO has unreserved stock and no Pick List is created against the SO.
-				if (
-					frm.doc.__onload &&
-					frm.doc.__onload.has_unreserved_stock &&
-					flt(frm.doc.per_picked) === 0
-				) {
-					frm.add_custom_button(
-						__("Reserve"),
-						() => frm.events.create_stock_reservation_entries(frm),
-						__("Stock Reservation")
-					);
-				}
-			}
-
-			// Stock Reservation > Unreserve button will be only visible if the SO has un-delivered reserved stock.
-			if (
-				frm.doc.__onload &&
-				frm.doc.__onload.has_reserved_stock &&
-				frappe.model.can_cancel("Stock Reservation Entry")
-			) {
-				frm.add_custom_button(
-					__("Unreserve"),
-					() => frm.events.cancel_stock_reservation_entries(frm),
-					__("Stock Reservation")
-				);
-			}
-
-			frm.doc.items.forEach((item) => {
-				if (flt(item.stock_reserved_qty) > 0 && frappe.model.can_read("Stock Reservation Entry")) {
-					frm.add_custom_button(
-						__("Reserved Stock"),
-						() => frm.events.show_reserved_stock(frm),
-						__("Stock Reservation")
-					);
-					return;
-				}
-			});
-		}
-
-		if (frm.doc.docstatus === 0) {
-			if (frm.doc.is_internal_customer) {
-				frm.events.get_items_from_internal_purchase_order(frm);
-			}
-
-			if (frm.doc.docstatus === 0) {
-				frappe.call({
-					method: "erpnext.selling.doctype.sales_order.sales_order.get_stock_reservation_status",
-					callback: function (r) {
-						if (!r.message) {
-							frm.set_value("reserve_stock", 0);
-							frm.set_df_property("reserve_stock", "read_only", 1);
-							frm.set_df_property("reserve_stock", "hidden", 1);
-							frm.fields_dict.items.grid.update_docfield_property("reserve_stock", "hidden", 1);
-							frm.fields_dict.items.grid.update_docfield_property(
-								"reserve_stock",
-								"default",
-								0
-							);
-							frm.fields_dict.items.grid.update_docfield_property(
-								"reserve_stock",
-								"read_only",
-								1
-							);
-						}
-					},
-				});
-			}
-		}
-
-		// Hide `Reserve Stock` field description in submitted or cancelled Sales Order.
-		if (frm.doc.docstatus > 0) {
-			frm.set_df_property("reserve_stock", "description", null);
-		}
-	},
-
-	get_items_from_internal_purchase_order(frm) {
-		if (!frappe.model.can_read("Purchase Order")) {
-			return;
-		}
-
-		frm.add_custom_button(
-			__("Purchase Order"),
-			() => {
-				erpnext.utils.map_current_doc({
-					method: "erpnext.buying.doctype.purchase_order.purchase_order.make_inter_company_sales_order",
-					source_doctype: "Purchase Order",
-					target: frm,
-					setters: [
-						{
-							label: __("Supplier"),
-							fieldname: "supplier",
-							fieldtype: "Link",
-							options: "Supplier",
-						},
-					],
-					get_query_filters: {
-						company: frm.doc.company,
-						is_internal_supplier: 1,
-						docstatus: 1,
-						status: ["!=", "Completed"],
-					},
-				});
-			},
-			__("Get Items From")
-		);
-	},
-
-	// When multiple companies are set up. in case company name is changed set default company address
-	company: function (frm) {
-		if (frm.doc.company) {
-			frappe.call({
-				method: "erpnext.setup.doctype.company.company.get_default_company_address",
-				args: {
-					name: frm.doc.company,
-					existing_address: frm.doc.company_address || "",
-				},
-				debounce: 2000,
-				callback: function (r) {
-					if (r.message) {
-						frm.set_value("company_address", r.message);
-					} else {
-						frm.set_value("company_address", "");
-					}
-				},
+	refresh: function(frm) {
+		if((frm.doc.docstatus === 1 && frm.doc.status !== 'Closed'
+			&& flt(frm.doc.per_delivered, 6) < 100 && flt(frm.doc.per_billed, 6) < 100)
+			|| (frm.doc.docstatus === 1 && frm.doc.status !== 'Closed'
+			&& flt(frm.doc.per_delivered, 6) < 100 || flt(frm.doc.per_billed, 6) < 100
+			&& frm.doc.quotation_type == "NCR")
+		) {
+			frm.add_custom_button(__('Update Items'), () => {
+				erpnext.utils.update_child_items({
+					frm: frm,
+					child_docname: "items",
+					child_doctype: "Sales Order Detail",
+					cannot_add_row: false,
+				})
 			});
 		}
 	},
-
-	onload: function (frm) {
-		if (!frm.doc.transaction_date) {
-			frm.set_value("transaction_date", frappe.datetime.get_today());
+	onload: function(frm) {
+		if (!frm.doc.transaction_date){
+			frm.set_value('transaction_date', frappe.datetime.get_today())
 		}
-		erpnext.queries.setup_queries(frm, "Warehouse", function () {
+		erpnext.queries.setup_queries(frm, "Warehouse", function() {
 			return {
-				filters: [["Warehouse", "company", "in", ["", cstr(frm.doc.company)]]],
+				filters: [
+					["Warehouse", "company", "in", ["", cstr(frm.doc.company)]],
+				]
 			};
+		});
+
+		frm.set_query('project', function(doc, cdt, cdn) {
+			return {
+				query: "erpnext.controllers.queries.get_project_name",
+				filters: {
+					'customer': doc.customer
+				}
+			}
 		});
 
 		frm.set_query("warehouse", "items", function (doc, cdt, cdn) {
