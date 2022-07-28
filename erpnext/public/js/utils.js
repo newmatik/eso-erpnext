4735fd2c51564af8c6b3d8bf87fd54e06b838f64
icecliff <isaiah@elexess.com>
2022-07-28 10:25:16 +0000

    fix(Sales Order Item): Add Required Date

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -879,9 +879,18 @@ erpnext.utils.update_child_items = function (opts) {
 		fields.splice(4, 0, {
 			fieldtype: "Float",
 			fieldname: "conversion_factor",
+			in_list_view: 0,
 			label: __("Conversion Factor"),
 			precision: get_precision("conversion_factor"),
 		});
+	}
+	if (frm.doc.doctype == 'Sales Order') {
+		fields.splice(2, 0, {
+			fieldtype: 'Date',
+			fieldname: "reqd_by_date",
+			in_list_view: 1,
+			label: __("Reqd By Date")
+		})
 	}
 
 	if (
@@ -971,11 +980,27 @@ erpnext.utils.update_child_items = function (opts) {
 			this.hide();
 			refresh_field("items");
 		},
-		primary_action_label: __("Update"),
+		primary_action_label: __('Update')
 	});
 
+	frm.doc[opts.child_docname].forEach(d => {
+		dialog.fields_dict.trans_items.df.data.push({
+			"docname": d.name,
+			"name": d.name,
+			"item_code": d.item_code,
+			"reqd_by_date": d.reqd_by_date,
+			"delivery_date": d.delivery_date,
+			"schedule_date": d.schedule_date,
+			"conversion_factor": d.conversion_factor,
+			"qty": d.qty,
+			"rate": d.rate,
+			"uom": d.uom
+		});
+		this.data = dialog.fields_dict.trans_items.df.data;
+		dialog.fields_dict.trans_items.grid.refresh();
+	})
 	dialog.show();
-};
+}
 
 erpnext.utils.map_current_doc = function (opts) {
 	function _map() {
