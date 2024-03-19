7008748fb0fe316fa77efdde2d9028a341eeb287
jeremythegreat01 <jeremyjamessipin@gmail.com>
2024-03-19 21:03:00 +0800

    feat(sales_order.py): update header delivery date based on min date of list

--- erpnext/selling/doctype/sales_order/sales_order.py
+++ erpnext/selling/doctype/sales_order/sales_order.py
@@ -142,11 +142,11 @@ class SalesOrder(SellingController):
 	def validate_delivery_date(self):
 		if self.order_type == "Sales" and not self.skip_delivery_note:
 			delivery_date_list = [d.delivery_date for d in self.get("items") if d.delivery_date]
-			max_delivery_date = max(delivery_date_list) if delivery_date_list else None
-			if (max_delivery_date and not self.delivery_date) or (
-				max_delivery_date and getdate(self.delivery_date) != getdate(max_delivery_date)
+			min_delivery_date = min(delivery_date_list) if delivery_date_list else None
+			if (min_delivery_date and not self.delivery_date) or (
+				min_delivery_date and getdate(self.delivery_date) != getdate(min_delivery_date)
 			):
-				self.delivery_date = max_delivery_date
+				self.delivery_date = min_delivery_date
 			if self.delivery_date:
 				for d in self.get("items"):
 					if not d.delivery_date:
