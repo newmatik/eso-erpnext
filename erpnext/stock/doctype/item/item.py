dd525f56918c1d5310ebebd36cf78e226d4f1014
Jeremy Sipin <jsi@newmatik.com>
2024-12-10 11:58:53 +0100

    feat: allow customer items as purchase item (#238)

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -134,10 +134,6 @@ class Item(Document):
 
 	def validate_customer_provided_part(self):
 		if self.is_customer_provided_item:
-			if self.is_purchase_item:
-				frappe.throw(_('"Customer Provided Item" cannot be Purchase Item also'))
-			if self.valuation_rate:
-				frappe.throw(_('"Customer Provided Item" cannot have Valuation Rate'))
 			self.default_material_request_type = "Customer Provided"
 
 	def add_price(self, price_list=None):
